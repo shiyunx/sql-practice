f7c16fa5237fd97010e5ad6d34eed2e56b95ed83
shiyun <shiyun@shiyuns-MacBook-Air.local>
2026-05-26 14:33:55 +0800

    edit

--- sql_leetcode/sql50/select/1.sql
+++ sql_leetcode/sql50/select/1.sql
@@ -50,4 +50,4 @@ Explanation: Only products 1 and 3 are both low fat and recyclable.
 
 SELECT product_id
 FROM Products
-WHERE low_fats = 'Y' AND recyclable = 'Y'
+WHERE low_fats = 'Y' AND recyclable = 'Y';
